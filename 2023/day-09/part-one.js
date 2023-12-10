1bcfe32f9adbec2188d99e06adc44b09b27d0839
mihael <mihic.medan@gmail.com>
2023-12-10 17:34:47 +0100

    day 09

---
+++ 2023/day-09/part-one.js
@@ -0,0 +1,47 @@
+const fs = require('node:fs');
+
+const inputs = fs.readFileSync('./inputs.txt')
+    .toString()
+    .split('\n')
+    .filter(line => line.length);
+
+let sum = 0;
+for (const line of inputs) {
+    sum += extrapolateValue(line);
+}
+console.log(sum);
+
+function extrapolateValue(input) {
+    const values = [input.split(' ').map(val => +val)];
+    let nextVal = [];
+
+    while (!noDiff(last(values))) {
+        let nextVal = [];
+        let val = Array.from(last(values));
+        for (let i = 1; i < val.length; i++) {
+            nextVal.push(val[i] - val[i - 1]);
+        }
+
+        values.push(Array.from(nextVal));
+    }
+
+    for (let i = values.length - 2; i >= 0; i--) {
+        values[i].push(last(values[i]) + last(values[i + 1]));
+    }
+    return last(values[0]);
+}
+
+function noDiff(arr) {
+    for (const item of arr) {
+        if (item !== 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function last(arr) {
+    return arr[arr.length - 1];
+}
+
